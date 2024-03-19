d9be2b55ba9edbb83357aeef27c134b72d570e9c
Hassan Cissey <hcissey0@gmail.com>
2024-03-19 12:49:02 +0000

    create user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,11 +12,13 @@ class UsersController {
     if (userExists) return res.status(400).send({ error: 'Already exist' });
 
     const hashedPassword = sha1(req.body.password);
+    console.log(hashedPassword);
     const result = await dbClient.db.collection('users').insertOne({
       email: req.body.email, password: hashedPassword,
     });
+    console.log(result);
 
-    return res.status(201).send({ id: result.insertId, email: req.body.email });
+    return res.status(201).send({ id: result.insertedId, email: req.body.email });
   }
 
   static async getMe(req, res) {
